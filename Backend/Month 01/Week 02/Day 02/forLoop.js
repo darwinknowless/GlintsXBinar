b567c859a791633adfbef657ed4e8e162caef81f
Darwin <id.darwins@gmail.com>
2021-03-09 13:55:06 +0700

    Add File

--- Backend/Month 01/Week 02/Day 02/forLoop.js
+++ Backend/Month 01/Week 02/Day 02/forLoop.js
@@ -9,3 +9,13 @@ for (let i = 0; i < 5; i ++) {
 // let a = 1;
 // a++;
 // consoloe.log(a);
+
+// Fibonacci
+let b = [0, 1];
+
+for (let i = 0; i < 7; i ++) {
+    console.log(`b[` + i + "] = " + b[i]);
+    console.log(`b[` + (i + 1) + "] = " + b[i + 1]);
+    b[i + 2] = b[i] + b[i +1];
+    console.log("hasil -> b[" + (i + 2) + "] = " + b[i + 2]);
+}
