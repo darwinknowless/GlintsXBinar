793705e243404281a25a47c1a6541b0e03b2f107
Darwin <id.darwins@gmail.com>
2021-03-09 13:35:11 +0700

    +

---
+++ Backend/Month 01/Week 02/Day 02/forLoop.js
@@ -0,0 +1,11 @@
+for (let i = 0; i < 5; i += 2) {
+    console.log("Hello World! " + "perulangan ke-" + i);
+}
+
+for (let i = 0; i < 5; i ++) {
+    console.log(i);
+}
+
+// let a =1;
+// a++;
+// consoloe.log(a);
