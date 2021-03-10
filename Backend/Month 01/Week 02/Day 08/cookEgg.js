8f1c1dd77658a7545aca549c203913b2b2b158f8
Darwin <id.darwins@gmail.com>
2021-03-10 14:56:56 +0700

    Edit

--- Backend/Month 01/Week 02/Day 08/cookEgg.js
+++ Backend/Month 01/Week 02/Day 08/cookEgg.js
@@ -1,42 +1,43 @@
-let fridge = ["Apple","Wortel","Milk","Orange","Leech","Cabbage"];
+let fridge = ["Apple", "Wortel", "Milk", "Orange", "Leech", "Cabbage", "Egg"];
 
 function goToKitchen() {
-    console.log("Go to kitchen");
-    }
+  console.log("Go to kitchen!");
+}
 
-function checkTheEggAvailability() {
-    console.clear();
-    console.log("Checking the egg availability...")
-  
-    // Check the egg availability
-    for (let i = 0; i < fridge.length; i++) {
-      if (fridge[i] == "Egg") {
-        console.log("There's an egg")
-        return false
-      }
-    }
-  
-    // If we complete the loop and there's no egg
-    console.log("There's no egg")
-    return true
-    }
+function checkEggAvailability() {
+  console.log("Check egg.....");
 
-// Function to handle if we have to go to market or not
-function goToTheMarket(shouldWe) {
-    if (shouldWe) {
-      console.log("Heading to the market");
-      console.log("Buying an egg");
-    }
+  if (!fridge.includes("Egg")) {
+    console.log("The egg does not exist!");
+    buyTheEgg();
+  } else {
+    console.log("The egg exists!");
   }
-  
-  // Function to prepare the fry
-  function prepareTheFry() {
-    console.log("Pouring the oil");
-    console.log("Lit up the stove");
-  }
-  
-  // Fry the egg
-  function fryTheEgg() {
-    console.log("Cook the egg");
-  }
-  
+}
+
+function buyTheEgg() {
+  console.log("Go to market....");
+  console.log("Buy the egg!");
+}
+
+function prepareTheFriyer() {
+  console.log("Prepare the friyer!");
+  console.log("Pour the oil!");
+  console.log("Lit up the stove");
+  console.log("Wait until the oil's getting hot");
+}
+
+function cookingTheEgg() {
+  console.log("Break your egg and cook it until it's cooked");
+}
+
+function cookTheEgg() {
+  goToKitchen();
+  checkEggAvailability();
+  prepareTheFriyer();
+  cookingTheEgg();
+
+  console.log("End!");
+}
+
+cookTheEgg();
