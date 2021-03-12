18399e05dc03044177bd28319799a1fd7c607b01
Darwin <id.darwins@gmail.com>
2021-03-12 15:14:47 +0700

    +

---
+++ Backend/Month 01/Week 02/Day 10/Assigment.js
@@ -0,0 +1,28 @@
+/*
+*ASSIGNMENT
+*Create an array that contains ["tomato", "broccoli", "kale", "cabbage", "apple"]
+*Loop the array to print something like this "tomato is a healthy food, it's definitely worth to eat."
+*Exclude apple because apple is not a Vegetable.
+*/
+
+// Declare array
+let veggie = ["tomato", "broccoli", "kale", "cabbage", "apple"];
+
+// manual ways
+for (var i = 0; i < veggie.length; i++) {
+    console.log(
+        `${veggie[i]} is healthy food, it's definitely worth to eat's!`
+    );
+}
+
+// Apple is not veggie
+for (var i = 0; i < veggie.length; i++) {
+    if (veggie[i] == "apple") {
+        console.log("Apple isn't veggie");
+    } else {
+        console.log(
+            `${veggie[i]} is healthy food, it's definitely worth to eat's!`
+        );
+    }
+}
+
