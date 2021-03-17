e83401ff1832e2445d74aa5d808d88f4db9f57b2
Darwin <id.darwins@gmail.com>
2021-03-17 16:29:51 +0700

    Add Day 13 File

--- Backend/Month 01/Week 03/Day 13/tryGeo.js
+++ Backend/Month 01/Week 03/Day 13/tryGeo.js
@@ -1,8 +1,22 @@
-const { Square, Rectangle } = require("./geometry");
+const { Square, Rectangle, Triangle } = require("./geometry");
+
+console.log(" = = = = = = = = = = SQUARE = = = = = = = = = = ");
 
 let squareOne = new Square(12);
 squareOne.calculateArea();
+squareOne.calculateCircumference();
+
+console.log(" = = = = = = = = = = RECTANGLE = = = = = = = = = = ");
 
 let rectangleOne = new Rectangle(11, 12);
 rectangleOne.calculateArea();
 rectangleOne.calculateCircumference();
+
+console.log(" = = = = = = = = = = TRIANGLE = = = = = = = = = = ");
+
+let triangleOne = new Triangle(15, 30);
+triangleOne.calculateArea();
+triangleOne.calculateCircumference();
+
+
+
