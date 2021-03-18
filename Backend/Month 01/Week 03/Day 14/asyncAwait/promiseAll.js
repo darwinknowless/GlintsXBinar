5429195fcfef144708484b65cec52e04f2cba174
Darwin <id.darwins@gmail.com>
2021-03-18 15:57:25 +0700

    +

--- Backend/Month 01/Week 03/Day 14/asyncAwait/promiseAll.js
+++ Backend/Month 01/Week 03/Day 14/asyncAwait/promiseAll.js
@@ -13,11 +13,20 @@ const readFile = (file, option) =>
 const readAllFiles = async () => { // async : agar fungsi await bisa jalan
     try {
         // await : system antri, mengeksekusi file content1 dulu hingga selesai baru lanjut file berikutnya
-        let content1 = await readFile("./contents/content1.txt", "utf-8");
-        let content2 = await readFile("./contents/content2.txt", "utf-8");
-        let content3 = content1 + content2;
-        console.log(content3);
+        // let content1 = await readFile("./contents/content1.txt", "utf-8");
+        // let content2 = await readFile("./contents/content2.txt", "utf-8");
+        // let content3 = content1 + content2;
+        // console.log(content3);
 
+        // Promise all
+        let data = await Promise.all([
+            readFile("./contents/content1.txt", "utf-8"),
+            readFile("./contents/content2.txt", "utf-8"),
+        ]);
+
+        let content = data[0] + data[1];
+
+        console.log(content);        
     } catch (e) { // catch : mirip if else dengan benefit mengecek error
         console.log("It's error!");
     }
