0d651966e3b4cfbbfa6fd46ae005204a6e34899a
Darwin <id.darwins@gmail.com>
2021-03-19 16:29:25 +0700

    +

--- Backend/Month 01/Week 03/Day 15/index.js
+++ Backend/Month 01/Week 03/Day 15/index.js
@@ -1,26 +1,13 @@
-const express = require("express");
-const app = express();
+const express = require("express"); // Import express
+const app = express(); // Make express app
+const helloRoute = require("./routes/helloRoute"); // Import HelloRoute
 
-app.get ("/", (req, res) => {
-    console.log("This is my first backend!");
-    res.send("Hello, Postman (GET)!");
-});
+/* If user go to http://localhost:3000
+it will go here */
+app.use("/", helloRoute);
 
-app.post ("/", (req, res) => {
-    console.log("This is example POST!");
-    res.send("Hello, POST!");
-});
-
-app.put ("/", (req, res) => {
-    console.log("This is example PUT!");
-    res.send("Hello, PUT!");
-});
-
-app.delete ("/", (req, res) => {
-    console.log("This is example DELETE!");
-    res.send("Hello, DELETE!");
-});
-
+// This is port for this server
 app.listen(3000, () => console.log("Server running on 3000!/(port 3000)"));
 // run node index.js di terminal vs code
 // di postmant baru bisa di send
+// 3000 adalah port
---
+++ Backend/Month 01/Week 03/Day 15/routes/helloRoute.js
@@ -0,0 +1,17 @@
+const express = require("express"); // Import express
+const router = express.Router(); // Make a router
+const HelloController = require("../controllers/helloController"); // Import HelloController
+
+// If user go to http://localhost:3000 (GET)
+router.get("/", HelloController.get);
+
+// If user go to http://localhost:3000 (POST)
+router.post("/", HelloController.post);
+
+// If user go to http://localhost:3000 (PUT)
+router.put("/", HelloController.put);
+
+// If user go to http://localhost:3000 (DELETE)
+router.delete("/", HelloController.delete);
+
+module.exports = router; // Export router
