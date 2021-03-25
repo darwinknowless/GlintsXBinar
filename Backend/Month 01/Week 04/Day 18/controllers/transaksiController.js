cdd9c39f5ad4ac4f75ac0512b63cda87f19fa3b5
Darwin <id.darwins@gmail.com>
2021-03-25 11:21:36 +0700

    +

--- Backend/Month 01/Week 04/Day 18/controllers/transaksiController.js
+++ Backend/Month 01/Week 04/Day 18/controllers/transaksiController.js
@@ -25,6 +25,57 @@ const getAll = (req, res) => {
   });
 };
 
+// Create data
+const create = (req, res) => {
+  // Find price of barang
+  let sqlFindBarang = "SELECT * FROM barang WHERE id = ?";
+
+  // Run sqlFindBarang
+  connection.query(sqlFindBarang, [req.body.id_barang], (err, results) => {
+    let price = eval(results[0].harga);
+    let total = eval(req.body.jumlah * price);
+
+    // Make sql query
+    let sqlCreate =
+      "INSERT INTO transaksi(id_barang, id_pelanggan, jumlah, total) VALUES (?, ?, ?, ?)";
+
+    // Run Query Create
+    connection.query(
+      sqlCreate,
+      [req.body.id_barang, req.body.id_pelanggan, req.body.jumlah, total],
+      (err, results) => {
+        // If error
+        if (err) {
+          return res.status(500).json({
+            message: "Internal Server Error",
+            error: err,
+          });
+        }
+
+        // If Success
+        let sqlSelect = `SELECT t.id, p.nama as nama_pelanggan, b.nama as nama_barang, b.harga, pem.nama as nama_pemasok, t.waktu, t.jumlah, t.total FROM transaksi t JOIN barang b ON t.id_barang = b.id JOIN pelanggan p ON p.id = t.id_pelanggan JOIN pemasok pem ON b.id_pemasok = pem.id WHERE t.id = ${results.insertId}`;
+
+        // Run Select Query
+        connection.query(sqlSelect, (err, results) => {
+          // If error
+          if (err) {
+            return res.status(500).json({
+              message: "Internal Server Error",
+              error: err,
+            });
+          }
+
+          // If success
+          return res.status(200).json({
+            message: "Success",
+            data: results[0],
+          });
+        });
+      }
+    );
+  });
+};
+
 // Delete Data
 const deleteData = (req, res) => {
   // Delete Query
@@ -45,4 +96,4 @@ const deleteData = (req, res) => {
   });
 };
 
-module.exports = { getAll, deleteData };
+module.exports = { getAll, create, deleteData };
--- Backend/Month 01/Week 04/Day 18/routes/transaksiRoutes.js
+++ Backend/Month 01/Week 04/Day 18/routes/transaksiRoutes.js
@@ -6,6 +6,7 @@ const transaksiController = require("../controllers/transaksiController");
 
 // Define routes
 router.get("/", transaksiController.getAll);
+router.post("/", transaksiController.create);
 router.delete("/:id", transaksiController.deleteData);
 
 module.exports = router;
