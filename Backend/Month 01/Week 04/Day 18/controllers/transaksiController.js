ab149ebcd7b56f9467c8a8e20d5646a1dd6736e4
Darwin <id.darwins@gmail.com>
2021-03-25 16:20:13 +0700

    +

--- Backend/Month 01/Week 04/Day 18/controllers/transaksiController.js
+++ Backend/Month 01/Week 04/Day 18/controllers/transaksiController.js
@@ -7,7 +7,7 @@ const getAll = (req, res) => {
   let sql =
     "SELECT t.id, p.nama as nama_pelanggan, b.nama as nama_barang, pem.nama as nama_pemasok, b.harga, t.waktu, t.jumlah, t.total FROM transaksi t JOIN barang b ON t.id_barang = b.id JOIN pelanggan p ON p.id = t.id_pelanggan JOIN pemasok pem ON b.id_pemasok = pem.id";
 
-  // Run query
+  // Run Query
   connection.query(sql, (err, results) => {
     // If error
     if (err) {
@@ -17,7 +17,7 @@ const getAll = (req, res) => {
       });
     }
 
-    // If no return
+    // If no error
     return res.status(200).json({
       message: "Success",
       data: results,
@@ -25,12 +25,14 @@ const getAll = (req, res) => {
   });
 };
 
-// Get One Data
+// Get one traksasksi data
 const getOne = (req, res) => {
-  let sqlGetOne = `SELECT t.id, p.nama as nama_pelanggan, b.nama as nama_barang, pem.nama as nama_pemasok, b.harga, t.waktu, t.jumlah, t.total FROM transaksi t JOIN barang b ON t.id_barang = b.id JOIN pelanggan p ON p.id = t.id_pelanggan JOIN pemasok pem ON b.id_pemasok = pem.id WHERE t.id = ${req.params.id}`;
+  // Get One Query
+  let sql = `SELECT t.id, p.nama as nama_pelanggan, b.nama as nama_barang, pem.nama as nama_pemasok, b.harga, t.waktu, t.jumlah, t.total FROM transaksi t JOIN barang b ON t.id_barang = b.id JOIN pelanggan p ON p.id = t.id_pelanggan JOIN pemasok pem ON b.id_pemasok = pem.id WHERE t.id = ${req.params.id}`;
 
-  connection.query(sqlGetOne, [req.params.id], (err, results) => {
-    // If error, it will go to here
+  // Run Query
+  connection.query(sql, (err, results) => {
+    // If error
     if (err) {
       return res.status(500).json({
         message: "Internal Server Error",
@@ -38,7 +40,7 @@ const getOne = (req, res) => {
       });
     }
 
-    // If no error, it will go here
+    // If no error
     return res.status(200).json({
       message: "Success",
       data: results[0],
@@ -48,23 +50,32 @@ const getOne = (req, res) => {
 
 // Create data
 const create = (req, res) => {
-  // Find price of barang
-  let sqlFindBarang = "SELECT * FROM barang WHERE id = ?";
+  // Find barang to get the price
+  let sqlFindBarang = `SELECT * FROM barang WHERE id = ${req.body.id_barang}`;
 
-  // Run sqlFindBarang
-  connection.query(sqlFindBarang, [req.body.id_barang], (err, results) => {
-    let price = eval(results[0].harga);
-    let total = eval(req.body.jumlah * price);
+  // Run Query
+  connection.query(sqlFindBarang, (err, results) => {
+    let price = eval(results[0].harga); // To get barang price
+    let total = eval(price * req.body.jumlah); // Calculate total of transaksi
 
-    // Make sql query
-    let sqlCreate =
-      "INSERT INTO transaksi(id_barang, id_pelanggan, jumlah, total) VALUES (?, ?, ?, ?)";
+    // Create transaksi
+    let sqlCreate = `INSERT INTO transaksi(id_barang, id_pelanggan, jumlah, total) VALUES (${req.body.id_barang}, ${req.body.id_pelanggan}, ${req.body.jumlah}, ${total})`;
 
-    // Run Query Create
-    connection.query(
-      sqlCreate,
-      [req.body.id_barang, req.body.id_pelanggan, req.body.jumlah, total],
-      (err, results) => {
+    // Run Query
+    connection.query(sqlCreate, (err, results) => {
+      // If error
+      if (err) {
+        return res.status(500).json({
+          message: "Internal Server Error",
+          error: err,
+        });
+      }
+
+      // If no error
+      let sqlSelect = `SELECT t.id, p.nama as nama_pelanggan, b.nama as nama_barang, pem.nama as nama_pemasok, b.harga, t.waktu, t.jumlah, t.total FROM transaksi t JOIN barang b ON t.id_barang = b.id JOIN pelanggan p ON p.id = t.id_pelanggan JOIN pemasok pem ON b.id_pemasok = pem.id WHERE t.id = ${results.insertId}`;
+
+      // Run select
+      connection.query(sqlSelect, (err, results) => {
         // If error
         if (err) {
           return res.status(500).json({
@@ -73,53 +84,41 @@ const create = (req, res) => {
           });
         }
 
-        // If Success
-        let sqlSelect = `SELECT t.id, p.nama as nama_pelanggan, b.nama as nama_barang, b.harga, pem.nama as nama_pemasok, t.waktu, t.jumlah, t.total FROM transaksi t JOIN barang b ON t.id_barang = b.id JOIN pelanggan p ON p.id = t.id_pelanggan JOIN pemasok pem ON b.id_pemasok = pem.id WHERE t.id = ${results.insertId}`;
-
-        // Run Select Query
-        connection.query(sqlSelect, (err, results) => {
-          // If error
-          if (err) {
-            return res.status(500).json({
-              message: "Internal Server Error",
-              error: err,
-            });
-          }
-
-          // If success
-          return res.status(200).json({
-            message: "Success",
-            data: results[0],
-          });
+        // If no error
+        return res.status(201).json({
+          message: "Success",
+          data: results[0],
         });
-      }
-    );
+      });
+    });
   });
 };
 
-// Update Data
+// Update data data
 const update = (req, res) => {
-  // Find barang
-  let sqlFindBarang = "SELECT * FROM barang WHERE id = ?";
+  // Find barang to get the price
+  let sqlFindBarang = `SELECT * FROM barang WHERE id = ${req.body.id_barang}`;
 
-  // Run find query
-  connection.query(sqlFindBarang, [req.body.id_barang], (err, results) => {
-    let price = eval(results[0].harga);
-    let total = eval(req.body.jumlah * price);
+  connection.query(sqlFindBarang, (err, results) => {
+    let price = eval(results[0].harga); // To get barang price
+    let total = eval(price * req.body.jumlah); // Calculate total of transaksi
 
-    let sqlUpdate =
-      "UPDATE transaksi SET id_barang = ?, id_pelanggan = ?, jumlah = ?, total = ? WHERE id = ?";
+    let sqlUpdate = `UPDATE transaksi SET id_barang = ${req.body.id_barang}, id_pelanggan = ${req.body.id_pelanggan}, jumlah = ${req.body.jumlah}, total = ${total} WHERE id = ${req.params.id}`;
 
-    connection.query(
-      sqlUpdate,
-      [
-        req.body.id_barang,
-        req.body.id_pelanggan,
-        req.body.jumlah,
-        total,
-        req.params.id,
-      ],
-      (err, results) => {
+    connection.query(sqlUpdate, (err, results) => {
+      // If error
+      if (err) {
+        return res.status(500).json({
+          message: "Internal Server Error",
+          error: err,
+        });
+      }
+
+      // If no error
+      let sqlSelect = `SELECT t.id, p.nama as nama_pelanggan, b.nama as nama_barang, pem.nama as nama_pemasok, b.harga, t.waktu, t.jumlah, t.total FROM transaksi t JOIN barang b ON t.id_barang = b.id JOIN pelanggan p ON p.id = t.id_pelanggan JOIN pemasok pem ON b.id_pemasok = pem.id WHERE t.id = ${req.params.id}`;
+
+      // Run select
+      connection.query(sqlSelect, (err, results) => {
         // If error
         if (err) {
           return res.status(500).json({
@@ -128,31 +127,17 @@ const update = (req, res) => {
           });
         }
 
-        // If success
-        let sqlGetOne =
-          "SELECT t.id, p.nama as nama_pelanggan, b.nama as nama_barang, b.harga, pem.nama as nama_pemasok, t.waktu, t.jumlah, t.total FROM transaksi t JOIN barang b ON t.id_barang = b.id JOIN pelanggan p ON p.id = t.id_pelanggan JOIN pemasok pem ON b.id_pemasok = pem.id WHERE t.id = ?";
-
-        connection.query(sqlGetOne, [req.params.id], (err, results) => {
-          // If error, it will go to here
-          if (err) {
-            return res.status(500).json({
-              message: "Internal Server Error",
-              error: err,
-            });
-          }
-
-          // If no error, it will go here
-          return res.status(200).json({
-            message: "Success",
-            data: results[0],
-          });
+        // If no error
+        return res.status(201).json({
+          message: "Success",
+          data: results[0],
         });
-      }
-    );
+      });
+    });
   });
 };
 
-// Delete Data
+// Delete data
 const deleteData = (req, res) => {
   // Delete Query
   let sql = "DELETE FROM transaksi WHERE id = ?";
