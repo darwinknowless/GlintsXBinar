dd7af74975f8f94c3eec63479b727c170a642860
Darwin <id.darwins@gmail.com>
2021-03-26 22:35:33 +0700

    + import router pemasok & define routes pemasok

--- Backend/Month 01/Week 04/Day 18/index.js
+++ Backend/Month 01/Week 04/Day 18/index.js
@@ -3,12 +3,14 @@ const app = express();
 
 // Import router
 const transaksiRoutes = require("./routes/transaksiRoutes");
+const pemasokRoutes = require("./routes/pemasokRoutes");
 
 // Use to read req.body
 app.use(express.urlencoded({ extended: true }));
 
 // Define routes
 app.use("/transaksi", transaksiRoutes);
+app.use("/pemasok", pemasokRoutes);
 
 // Server running
 let PORT = 3000 || process.env.PORT;
