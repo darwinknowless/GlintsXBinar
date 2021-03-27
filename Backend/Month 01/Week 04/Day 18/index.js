817e3dc4368af4dcad2ff6175e2171fe9b038933
Darwin <id.darwins@gmail.com>
2021-03-27 16:53:05 +0700

    edit index

--- Backend/Month 01/Week 04/Day 18/index.js
+++ Backend/Month 01/Week 04/Day 18/index.js
@@ -1,17 +1,18 @@
+//Import express
 const express = require("express");
 const app = express();
 
-// Import router
+//Import router
 const transaksiRoutes = require("./routes/transaksiRoutes");
 const pemasokRoutes = require("./routes/pemasokRoutes");
 
-// Use to read req.body
+//Use to read req.body
 app.use(express.urlencoded({ extended: true }));
 
-// Define routes
+//Define routes
 app.use("/transaksi", transaksiRoutes);
 app.use("/pemasok", pemasokRoutes);
 
-// Server running
+//Server running
 let PORT = 3000 || process.env.PORT;
 app.listen(PORT, () => console.log(`Server running on ${ PORT }!`));
