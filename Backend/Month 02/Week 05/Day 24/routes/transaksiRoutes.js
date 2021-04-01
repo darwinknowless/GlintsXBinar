8a3bf7a26dcbf0e46858109093e5d80f978a30ab
Darwin <id.darwins@gmail.com>
2021-04-01 15:40:59 +0700

    Belajar Mongoose

--- Backend/Month 02/Week 05/Day 24/routes/transaksiRoutes.js
+++ Backend/Month 02/Week 05/Day 24/routes/transaksiRoutes.js
@@ -3,12 +3,12 @@ const express = require("express");
 // Import validator
 
 // Import controller
-// const transaksiController = require("../controllers/transaksiController");
+const transaksiController = require("../controllers/transaksiController");
 
 // Make router
 const router = express.Router();
 
 // Get all transaksi data
-// router.get("/", transaksiController.getAll);
+router.get("/", transaksiController.getAll);
 
 module.exports = router;
