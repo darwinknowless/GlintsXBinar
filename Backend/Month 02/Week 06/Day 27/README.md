b0474399c01299c4ee52065e15ce677b062bc7ec
Darwin <id.darwins@gmail.com>
2021-04-06 15:06:31 +0700

    Day 27, Mongoose with Authentication feature

---
+++ Backend/Month 02/Week 06/Day 27/README.md
@@ -0,0 +1,5 @@
+1. npm install --save bcrypt jsonwebtoken passport passport-jwt passport-local
+
+--bcrypt : encrypt pass
+--jsonwebtoken : nomor tiket
+--passport : login
---
+++ Backend/Month 02/Week 06/Day 27/models/user.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose"); // Import mongoose
+const mongooseDelete = require("mongoose-delete"); // Package to enable soft delete
+
+const UserSchema = new mongoose.Schema(
+  {
+    nama: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    role: {
+      type: String,
+      default: "user",
+      required: true,
+    },
+  },
+  {
+    // Enable timestamps
+    timestamps: {
+      createdAt: "createdAt",
+      updatedAt: "updatedAt",
+    },
+  }
+);
+
+// Enable soft delete
+UserSchema.plugin(mongooseDelete, { overrideMethods: "all" });
+
+module.exports = mongoose.model("user", UserSchema); // Export model
