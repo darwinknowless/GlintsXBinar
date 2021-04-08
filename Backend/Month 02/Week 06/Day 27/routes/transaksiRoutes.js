e7badde2e8f7bf2e22f7d378e1e7a4987b951c49
Darwin <id.darwins@gmail.com>
2021-04-08 19:19:19 +0700

    Authentication & Authorization

--- Backend/Month 02/Week 06/Day 27/routes/transaksiRoutes.js
+++ Backend/Month 02/Week 06/Day 27/routes/transaksiRoutes.js
@@ -12,19 +12,49 @@ const auth = require("../middlewares/auth");
 // Make router
 const router = express.Router();
 
-// Get all transaksi
-router.get("/", auth.admin, transaksiController.getAll);
+//TODO: One by One Routes (see below)
+// // Get all transaksi
+// router.get(
+//     "/",
+//     auth.admin,
+//     transaksiController.getAll
+//     );
+// // Get one transaksi
+// router.get(
+//     "/:id",
+//     auth.user,
+//     transaksiValidator.getOne,
+//     transaksiController.getOne
+//     );
+// // Create transaksi
+// router.post(
+//     "/",
+//     transaksiValidator.create,
+//     transaksiController.create
+//     );
+// // Update transaksi
+// router.put(
+//     "/:id",
+//     transaksiValidator.update,
+//     transaksiController.update
+//     );
+// // Delete transaksi
+// router.delete(
+//     "/:id",
+//     transaksiValidator.delete,
+//     transaksiController.delete
+//     );
 
-// Get one transaksi
-router.get("/:id", auth.user, transaksiValidator.getOne, transaksiController.getOne);
+//TODO: Grouping Routes (see below)
+router
+  .route("/")
+  .get(auth.user, transaksiController.getAll)
+  .post(auth.admin, transaksiValidator.create, transaksiController.create);
 
-// Create transaksi
-router.post("/", transaksiValidator.create, transaksiController.create);
-
-// Update transaksi
-router.put("/:id", transaksiValidator.update, transaksiController.update);
-
-// Delete transaksi
-router.delete("/:id", transaksiValidator.delete, transaksiController.delete);
+router
+  .route("/:id")
+  .get(auth.user, transaksiValidator.getOne, transaksiController.getOne)
+  .put(auth.admin, transaksiValidator.update, transaksiController.update)
+  .delete(auth.admin, transaksiValidator.delete, transaksiController.delete);
 
 module.exports = router;
